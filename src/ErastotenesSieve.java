444ffcd5c7c11bc696718545e8ca1af06c827189
Szymon Zackiewicz <zacszym@gmail.com>
2022-10-15 16:44:35 +0200

    optimization

--- src/ErastotenesSieve.java
+++ src/ErastotenesSieve.java
@@ -3,22 +3,27 @@ import java.util.ArrayList;
 class ErastotenesSieve {
 
     static ArrayList<Integer> erastotenesSieve(int max) {
-        int[] tab=new int[max+1];
+        int[] tab = new int[max + 1];
+
         for (int i = 1; i <= max; i++)
-            tab[i]=i;
+            tab[i] = i;
 
-        for (int j = 2; j <= max; j++) {
-            int k = 2;
-            while (j * k <= max) {
+        var primes = new ArrayList<Integer>();
 
-                tab[j*k]=0;
+        int p = 2;
+
+        while (p < max) {
+
+            primes.add(p);
+            tab[p] = 0;
+            int k = p;
+            while (p * k <= max) {
+
+                tab[p * k] = 0;
                 k++;
             }
-        }
-        var primes=new ArrayList<Integer>();
-        for(int i=2;i<max+1;i++){
-            if(tab[i]!=0)
-                primes.add(tab[i]);
+            while (p < max && tab[p] == 0)
+                p++;
         }
         return primes;
     }
